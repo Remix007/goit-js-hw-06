e12e21ea04fef69cc4ac487d9fdc87ba6cd1ece9
Remix007 <forvard16back@gmail.com>
2022-08-24 03:30:51 +0300

    update

--- js/task-06.js
+++ js/task-06.js
@@ -22,19 +22,15 @@
 //   border-color: #f44336;
 // }
 
-const textInputEl = document.querySelector('input');
-const inputLengthEl = document.querySelector('input[data-length ="6"]');
+const inputEl = document.querySelector("#validation-input");
 
-console.log(textInputEl);
-
-const blueredInputValidation = () => {
-  if (textInputEl.value.length === Number(inputLengthEl.dataset.length)) {
-    textInputEl.classList.add('valid');
-    textInputEl.classList.remove('invalid');
+const onInputBlur = () => {
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
   } else {
-    textInputEl.classList.add('invalid');
-    textInputEl.classList.remove('valid');
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 };
-console.log(inputLengthEl.dataset.length);
-textInputEl.addEventListener('blur', blueredInputValidation);
+inputEl.addEventListener("blur", onInputBlur);
