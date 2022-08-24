371c7fb587e6af066bec5474f7818677f71d7895
Remix007 <forvard16back@gmail.com>
2022-08-24 03:28:25 +0300

    fix input

--- js/task-06.js
+++ js/task-06.js
@@ -22,27 +22,19 @@
 //   border-color: #f44336;
 // }
 
-const refs = {
-    input: document.querySelector("#validation-input"),
-  };
-  const totalLength = refs.input.getAttribute("data-length");
-  // console.log(totalLength);
-  const intTotallenght = parseInt(totalLength, 10);
-  // console.log(intTotallenght);
-  
-  refs.input.addEventListener("focus", onInputFocus);
-  refs.input.addEventListener("blur", onInputBlur);
-  
-  function onInputFocus() {
-    console.log("Включив фокус");
+const textInputEl = document.querySelector('input');
+const inputLengthEl = document.querySelector('input[data-length ="6"]');
+
+console.log(textInputEl);
+
+const blueredInputValidation = () => {
+  if (textInputEl.value.length === Number(inputLengthEl.dataset.length)) {
+    textInputEl.classList.add('valid');
+    textInputEl.classList.remove('invalid');
+  } else {
+    textInputEl.classList.add('invalid');
+    textInputEl.classList.remove('valid');
   }
-  function onInputBlur() {
-    console.log("Інпут втратив фокус - blur");
-    if (refs.input.value.length === intTotallenght) {
-      refs.input.classList.remove("invalid");
-      refs.input.classList.add("valid");
-    } else if (refs.input.value.length !== intTotallenght) {
-      refs.input.classList.remove("valid");
-      refs.input.classList.add("invalid");
-    }
-  }
+};
+console.log(inputLengthEl.dataset.length);
+textInputEl.addEventListener('blur', blueredInputValidation);
