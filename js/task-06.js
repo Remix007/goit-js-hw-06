e05fe042c21669c2e8290632acf8c67cf48bf138
Remix007 <forvard16back@gmail.com>
2022-08-24 03:38:06 +0300

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,38 +1,16 @@
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
-
-// <input
-//   type="text"
-//   id="validation-input"
-//   data-length="6"
-//   placeholder="Please enter 6 symbols"
-// />
-// Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
-// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
-
-// #validation-input {
-//   border: 3px solid #bdbdbd;
-// }
-
-// #validation-input.valid {
-//   border-color: #4caf50;
-// }
-
-// #validation-input.invalid {
-//   border-color: #f44336;
-// }
-
-const inputForm = document.querySelector("#validation-input");
-
-inputForm.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.length == inputForm.dataset.length) {
-        inputForm.classList.add("valid")
-        inputForm.classList.remove("invalid")
-    } else if (event.currentTarget.value.length == "") {
-        inputForm.classList.remove("valid")
-        inputForm.classList.remove("invalid")
+const validationInputRef = document.querySelector("#validation-input");
+validationInputRef.addEventListener("blur", () => {
+    const valueLength = validationInputRef.value.length;
+  
+    if(valueLength === 0) {
+        validationInputRef.classList.remove("invalid");
+        validationInputRef.classList.remove("valid"); 
+    }else if(parseInt(validationInputRef.getAttribute("data-length")) === valueLength) {
+        
+        validationInputRef.classList.add("valid");
+        validationInputRef.classList.remove("invalid"); 
     } else {
-        inputForm.classList.remove("valid") 
-        inputForm.classList.add("invalid")
+        validationInputRef.classList.add("invalid");
+        validationInputRef.classList.remove("valid");
     }
 })
